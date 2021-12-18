6947b30dba6c65c0fc6c77b7d93d6559842381c3
Stella Gurin <59928550+stellagurin@users.noreply.github.com>
2021-12-18 15:07:30 -0500

    Added Quiz1.py

---
+++ Quiz1.py
@@ -0,0 +1,22 @@
+# Assign a value to x such that it evaluates to the product of 42 and pi (as defined in the math module).
+
+import math
+x = 42 *  math.pi
+
+# Given the provided statements, insert the correct combination of statements so that z is a float with value 14.0.
+
+x = 7
+y = 7.0
+z = x + y
+
+# What value does the following expression evaluate to?
+
+print (42 == '42') # False
+
+# What value does the following expression evaluate to?
+
+print (42 == 42.0) # True
+
+# What value does the following expression evaluate to?
+
+print (type(42) == type(42.0)) # False
