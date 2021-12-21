58eb9d19034b49815cfee194d9a5662c6ca545b4
Stella Gurin <59928550+stellagurin@users.noreply.github.com>
2021-12-21 18:10:15 -0500

    Add Quiz2.py

---
+++ Quiz2.py
@@ -0,0 +1,15 @@
+print(not ('hello' == 'HELLO')) # True
+
+print((32 > 15) and (56 == '56')) # False
+
+print(2**3 + 5 // 2) # 10
+
+print(len('Hi'*3+'python')) # 12
+
+print(type(34) == type('34')) # False
+
+print(37 >= 37.01 or not (45 <= 45.000)) # False
+
+print(type(34) == type(int('34'))) # True
+
+print('hello' == 'Hello') # False
